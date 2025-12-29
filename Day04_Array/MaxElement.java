a60b8e6c8cc212189f995256e4556f2b8fe00e80
Aditi-Sampate <aditisampate@gmail.com>
2025-12-29 23:27:10 +0530

    Add Java program to find largest element in an array

---
+++ Day04_Array/MaxElement.java
@@ -0,0 +1,19 @@
+//Q.Write a java program to find the largest element in an array.
+
+class MaxElement
+{
+	public static void main(String[] args) 
+	{
+		int []arr = {12,45,7,89,23};
+		int max = arr[0];
+
+		for(int i=1; i<arr.length; i++)
+		{
+			if(arr[i] > max)
+			{
+				max = arr[i];
+			}
+		}
+		System.out.println("Maximum Element :"+max);
+	}
+}
