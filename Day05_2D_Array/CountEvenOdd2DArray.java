17d247c8489c8bdedb5db524dd1728e476250fa7
Aditi-Sampate <aditisampate@gmail.com>
2026-01-02 23:04:02 +0530

    Implemented The 2D Array To Count Total Number Of Even And Odd Numbers In Array

---
+++ Day05_2D_Array/CountEvenOdd2DArray.java
@@ -0,0 +1,34 @@
+//Q. Write a program to count the even and odd numbers.
+
+class CountEvenOdd2DArray{
+
+	public static void main(String[] args) {
+		int[][] arr = {
+
+			{10,20,30},
+			{43,54,98},
+			{76,87,98}
+		};
+
+		int evenCount = 0;
+		int oddCount = 0;
+
+		for(int i=0; i<arr.length; i++)
+		{
+			for(int j=0; j<arr[i].length;j++)
+			{
+				if(arr[i][j] % 2 == 0)
+				{
+					evenCount++;
+				}
+				else
+				{
+					oddCount++;
+				}
+			}
+		}
+		System.out.println("Total Even Number :"+evenCount);
+		System.out.println("Total Odd Numbers :"+oddCount);
+	}
+	
+}
