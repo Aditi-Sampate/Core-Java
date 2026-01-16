9eef2965e6406054841f9c57a509ac58c5482f2a
Aditi-Sampate <aditisampate@gmail.com>
2026-01-16 21:27:57 +0530

    Add basic string example and length method

---
+++ Day07_String/StringBasic.java
@@ -0,0 +1,10 @@
+class StringBasic
+{
+	public static void main(String[] args) {
+		
+		String name = "Aditi";
+
+		System.out.println("Name :"+name);
+		System.out.println("Length :"+name.length());
+	}
+}
